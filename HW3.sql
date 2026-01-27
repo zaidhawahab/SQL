d6cdae3878ab2f0fa1ee211c777da7b4ff8e74ba
ZAIDHA WAHAB <86358236+zaidhawahab@users.noreply.github.com>
2026-01-27 15:12:04 +0530

    Update and rename CW3.sql to HW3.sql

--- HW3.sql
+++ HW3.sql
@@ -2,6 +2,7 @@ INSERT INTO students1
 VALUES (1, 'Alice', 'Web Development', 5000, 'Inactive'),
 (2, 'Bob', 'Data Science', 7000, 'Inactive'),
 (3, 'Charlie', 'UI/UX Design', 4000, 'Active');
+
 SELECT * FROM students1
 WHERE fees_paid>5000; 
 
@@ -18,6 +19,8 @@ SET status='Inactive',fees_paid=fees_paid-500
 WHERE id=3;
 
 DELETE FROM students1
-WHERE id=2;DELETE FROM students1
+WHERE id=2;
+
+DELETE FROM students1
 WHERE status='Inactive';
 
